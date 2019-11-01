dc76d4963a703f3d62dd0e10bbcbd7eef87104fc
Cole Webb <cole.webb@outlook.com>
2019-11-01 11:48:07 -0600

    added specs

--- Red.java
+++ Red.java
@@ -1,6 +1,19 @@
 // Specs
 // 
-// - 
+// - due last week of class, for presentations
+// - two types of javafx panes
+// - six types of nodes
+// - an animation
+// - demonstrate events, bindings and listeners
+// - server -> client or client -> client communication
+//      - No extra component for web browser
+//
+// Web browser specifics:
+// - Address bar, with ability to type a web address and load it
+// - Allow a user to browse a website
+// - Dropdown menu for adding favorite sites
+// - Clicking on a link in dropdown loads it
+// - Back button with 1-deep history (only load the previous site)
 
 import java.util.ArrayList;
 import javafx.application.Application;
