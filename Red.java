0ad163e28552736eb8cd7b513f90bd7b07ea9e24
Cole Webb <cole.webb@outlook.com>
2019-11-08 14:16:22 -0700

    added search functionality
    if the address in the address box isn't a web address, does a google search instead.

--- Red.java
+++ Red.java
@@ -2,9 +2,9 @@
 // 
 // - due last week of class, for presentations
 // - two types of javafx panes                      (done)
-// - six types of nodes
-// - an animation
-// - demonstrate events, bindings and listeners
+// - six types of nodes                             (done)
+// - an animation                                   (done)
+// - demonstrate events, bindings and listeners     (done)
 //
 // Web browser specifics:
 // - Address bar, with ability to type a web address and load it        (done)
@@ -12,27 +12,38 @@
 // - Dropdown menu for adding favorite sites                            (done)
 // - Clicking on a link in dropdown loads it                            (done)
 // - Back button with 1-deep history (only load the previous site)
+// - Tabbed UI
+// - Save bookmarks to file for permeance                               (done)
+// - If string typed in address bar isn't a web address, web search it
 
 import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
-import javafx.scene.layout.GridPane;
+import javafx.util.Duration;
+import javafx.scene.layout.GridPane;            // pane 1
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.Pane;                // pane 2
 import javafx.scene.layout.VBox;
-import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.control.TextField;      // node 1
 import javafx.scene.input.KeyCode;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.web.WebView;
+import javafx.scene.Scene;                  // node 2
+import javafx.scene.control.Button;         // node 3
+import javafx.scene.control.ComboBox;       // node 4
+import javafx.scene.control.Label;          // node 5
+import javafx.scene.web.WebView;            // node 6
 
 public class Red extends Application {
     GridPane p = new GridPane();                                // new GridPane
@@ -46,7 +57,8 @@ public class Red extends Application {
     HBox bottom = new HBox();
     Label title = new Label();
     Button about = new Button("About");
-
+    String[] history = new String[2];
+    Pattern urlCheck = Pattern.compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
     public void read () {                           // method to read bookmarks from file and populate local variables
         File open = new File("./bookmarks.txt");    // open the file
         try {                                       // 
@@ -84,13 +96,29 @@ public class Red extends Application {
         view.getEngine().load(url);                             //
     }
 
+    public void onPageLoad () {
+        String location = view.getEngine().getLocation();
+        for (String newString : history) {
+            if (location.equals(newString)) {
+                return;
+            }
+        }
+        if (history[1] != null) {
+            history[0] = history[1];
+            history[1] = location;
+        }
+        else {
+            history[1] = location;
+        }
+    }
+
     protected String getLocation () {                           // method for getting current location
         return view.getEngine().getLocation();                  //
     }
     
     public void start(Stage st) {                               // start
         read();                                                 //
-        bottom.setAlignment(Pos.CENTER_LEFT);
+        bottom.setAlignment(Pos.CENTER_LEFT);                   // 
         ObservableList<String> forCbo =                         //
             FXCollections.observableArrayList(bookmarkList);    //
         cbo.getItems().addAll(forCbo);                          //
@@ -104,7 +132,10 @@ public class Red extends Application {
             address.setText(view.getEngine().getLocation());    // set the address bar text
         });                                                     // end of combobox lambda
 
-        // TODO: bind address bar to current location in the webview
+        st.widthProperty().addListener(ov -> {
+            cbo.setMaxWidth(st.getWidth() / 8);
+        });
+
         bookmarks.setOnMouseClicked(e -> {                          // bookmark button lambda
             String toBookmark = view.getEngine().getLocation();     // get the current location
             bookmarkList.add(toBookmark);                           // add the location to the list
@@ -113,18 +144,41 @@ public class Red extends Application {
         });                                                         // end bookmark button lambda
 
         back.setOnMouseClicked(e -> {                               // back button lambda TODO: broken back button
+            this.onPageLoad();
+            view.getEngine().load(history[0]);
         });                                                         // end of back button lambda
 
         address.setOnKeyPressed(e -> {                          // address textfield lambda
             if (e.getCode() == KeyCode.ENTER) {                 // 
-                view.getEngine().load(address.getText());       //
+                Matcher toCheck = urlCheck.matcher(address.getText());
+                if (toCheck.matches()) {
+                    view.getEngine().load(address.getText());   //
+                }
+                else {
+                    String[] search = address.getText().split("\\s+");
+                    String google = "http://www.google.com/search?q=";
+                    for (String i : search) {
+                        google = google + i + "+";
+                    }
+                    view.getEngine().load(google);
+                }
+                
             }                                                   // 
         });                                                     // end of address textfield lambda
         about.setOnMouseClicked(e -> {
             Stage about = new Stage();                              // new stage
             Pane aboutPane = new Pane();                            // new pane
             VBox vb = new VBox();                                   // new vbox
-            // TODO: find a red animated logo for this thing
+            Circle icon = new Circle(75);
+            FadeTransition ft = new 
+                FadeTransition(Duration.millis(3000), icon);
+            ft.setFromValue(1.0);
+            ft.setToValue(0.0);
+            ft.setCycleCount(Timeline.INDEFINITE);
+            ft.setAutoReverse(true);
+            ft.play();
+            icon.setFill(Color.RED);
+            vb.getChildren().add(icon);
             vb.getChildren().add(new Label("Red Web Browser"));     // label
             vb.getChildren().add(new Label("'Pathetic Penguin'"));  // label
             vb.getChildren().add(new Label("This is free software, distributed without any promise of \nsupport or fitness for a specific task. Licensed under the \nGNU Public License v3."));
@@ -138,12 +192,13 @@ public class Red extends Application {
         });
         view.setOnMouseClicked(e -> {                       // link clicked lambda TODO: don't add the address more than once in a row
             String toGo = view.getEngine().getLocation();   // get current location
+            this.onPageLoad();
             address.setText(toGo);                          // set address bar
         });                                                 // end of link clicked lambda
         title.textProperty().bind(view.getEngine().titleProperty());    // bind the title of the webpage to a label
         bottom.getChildren().add(about);                                // add the about button the bottom
         bottom.getChildren().add(title);                                // add the title label to the bottom
-        cbo.setMaxWidth(150);           // set the width of the combobox
+        // cbo.setMaxWidth(150);           // set the width of the combobox
         p.add(bookmarks, 0, 0, 1, 1);   // add bookmark button
         p.add(back, 1, 0, 1, 1);        // add the back button
         p.add(address, 2, 0, 5, 1);     // add the address bar
